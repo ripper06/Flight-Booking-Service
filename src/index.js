e2acde96f6ace3baa7c565c881bd110c22f1a352
ripper06 <jyotiranjan.pvt@gmail.com>
2025-08-24 18:04:12 +0530

    Setup rabbit mq server and tested it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const amqplib = require('amqplib');
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+
+        await channel.assertQueue("notification-queue");
+
+        channel.sendToQueue("notification-queue", Buffer.from("one more msg 123"));
+        // setInterval(function(){
+        //     channel.sendToQueue("notification-queue", Buffer.from("this is msg"));
+        // },1000);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const {ServerConfig} = require('./config');
 const apiRoutes = require('./routes');
@@ -10,10 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
-// app.use('/bookingService/api', apiRoutes);
+app.use('/bookingService/api', apiRoutes);
 
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async()=>{
     console.log(`Successfully server started on port : ${ServerConfig.PORT}`);
     CRON();
+    await connectQueue();
+    console.log("Queue Connected!")
 });
